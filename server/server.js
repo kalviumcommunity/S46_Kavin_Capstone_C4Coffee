b08a04e4827b7efc8129bafd1492dbe69c1d4dfe
kavin0m <kavinsai88@gmail.com>
2024-04-24 14:27:33 +0530

    Connected to db and created user schema

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 
 const userRoute = require("./routes/userRouter");
+const { connectToDB, checkConnection } = require("./config/db");
 
 const app = express();
 
@@ -20,9 +21,16 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Server is running" });
 });
 
+app.get("/db", (req, res) => {
+  res.status(200).json({
+    connectionStatus: checkConnection() ? "Connected" : "Not Connected",
+  });
+});
+
 app.use("/user", userRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
+  connectToDB();
   console.log(`Server running on port ${PORT}`);
 });
